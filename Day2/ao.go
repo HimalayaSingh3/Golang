7eb08e49974ce3274b698d7da11f9743f91752a6
himalayasingh337 <himalayasingh337@gmail.com>
2025-04-22 00:14:24 +0530

    day3

---
+++ Day2/ao.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main(){
+
+	fmt.Println("Sum = " ,2 + 4)
+	fmt.Println("Subtraction = " , 4 - 2)
+	fmt.Println("Divide = " ,6 / 2)
+	fmt.Println("Multiplication = " ,2 * 4)
+}
---
+++ Day2/sum.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main(){
+	var a int = 2
+	var b int = 5
+
+	sum := a + b
+
+	fmt.Println("Sum is = ",sum)
+}
